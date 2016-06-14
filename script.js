1956a4d7fa5a34800c5f94fb9a6299f8af277f45
Booligoosh <booligoosh@optusnet.com.au>
2016-06-15 08:52:11 +1000

    used different repeating technique

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ var phi = 0;
 var fibnums = [];
 var currentNum = 0;
 
+start();
+
 function start() {
-  while (1 === 1) {
-    currentNum = currentNum + 1;
-    fibnum = fibnums[currentNum - 1] + fibnums[currentNum - 2];
-    fibnums.push(fibnum);
-    phi = fibnums[currentNum] / fibnums[currentNum - 1];
-  }
+  var x = 0;  // 0 Seconds
+  cycle();
+  setTimeout(start, x*1000);
 }
 
+function cycle() {
+  currentNum = currentNum + 1;
+  fibnum = fibnums[currentNum - 1] + fibnums[currentNum - 2];
+  fibnums.push(fibnum);
+  phi = fibnums[currentNum] / fibnums[currentNum - 1];
+}
