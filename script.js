13fdb7f05085bc6fb21c0c0482dbd7a52ea761d5
Booligoosh <booligoosh@optusnet.com.au>
2016-06-14 14:08:10 +1000

    added start() function

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ var phi = 0;
 var fibnums = [];
 var currentNum = 0;
 
-
-while (1 === 1) {
-  currentNum = currentNum + 1;
-  fibnum = fibnums[currentNum - 1] + fibnums[currentNum - 2];
-  fibnums.push(fibnum);
-  phi = fibnums[currentNum] / fibnums[currentNum - 1];
+function start() {
+  while (1 === 1) {
+    currentNum = currentNum + 1;
+    fibnum = fibnums[currentNum - 1] + fibnums[currentNum - 2];
+    fibnums.push(fibnum);
+    phi = fibnums[currentNum] / fibnums[currentNum - 1];
+  }
 }
+
